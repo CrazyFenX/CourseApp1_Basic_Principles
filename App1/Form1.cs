49f9163ac6beae959fe308a8063b7f7b031c74a0
Кирилл <kattoshkin@gmail.com>
2021-07-17 01:55:25 +0700

    Home

--- App1/Form1.cs
+++ App1/Form1.cs
@@ -22,6 +22,8 @@ namespace App1
         // * - умножение    S *= a     S = S * a
         // / - деление      S /= a     S = S / a
         // % - остаток      S %= a     S = S % a
+
+        //Посчитать сумму всех четных цифр числа
         private void startButton_Click(object sender, EventArgs e)
         {
             string inputStr = inputTextBox.Text;
@@ -38,6 +40,7 @@ namespace App1
             outputTextBox.Text = sum.ToString();
         }
 
+        //Вывести на экран все числа между А и В, подсчитать их количество
         private void startButton2_Click(object sender, EventArgs e)
         {
             int A = Convert.ToInt32(inputATextBox.Text);
@@ -55,5 +58,21 @@ namespace App1
 
             outputTextBox2.Text = output;
         }
+
+        //Домашка
+//      Циклы
+//          1)Вывести на экран квадраты чисел от А до В
+//      Строки
+//          2)Дан текст. Найти сумму имеющихся в нем цифр
+//   ------------------------
+//      Для тех, кому не хватило
+
+//          1)Найти суму корней из чисел от 1 до Н
+//          2)Найти суму кубов из чисел от A до B
+//          3)Найти количество чисел не кратных Н на промежутке от А до В
+//          4)Дан текст.Найдите наибольшее количество подряд идущих пробелов в нем
+
+//      http://www.itmathrepetitor.ru/prog/zadachi-na-stroki/
+//      http://codder.info/primery-resheniya-zadach-tsikl-for/
     }
 }
