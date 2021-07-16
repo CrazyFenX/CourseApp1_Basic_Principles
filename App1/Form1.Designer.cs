f23e5121b7cc7046fa66ba7c9ed0bd7dd46b82aa
User <User@WIN-SBDBR4LLI84>
2021-07-17 01:29:56 +0700

    Добавьте файлы проекта.

---
+++ App1/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿
+namespace App1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.inputTextBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.startButton3 = new System.Windows.Forms.Button();
+            this.outputTextBox3 = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.inputATextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.startButton2 = new System.Windows.Forms.Button();
+            this.outputTextBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.inputBTextBox = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inputTextBox.Location = new System.Drawing.Point(43, 57);
+            this.inputTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(132, 34);
+            this.inputTextBox.TabIndex = 0;
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startButton.Location = new System.Drawing.Point(43, 100);
+            this.startButton.Margin = new System.Windows.Forms.Padding(4);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(133, 42);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.outputTextBox.Location = new System.Drawing.Point(301, 57);
+            this.outputTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.Size = new System.Drawing.Size(184, 34);
+            this.outputTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(38, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ввод";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(295, 23);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Вывод";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.inputTextBox);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.startButton);
+            this.panel1.Controls.Add(this.outputTextBox);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(697, 156);
+            this.panel1.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.inputTextBox3);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.startButton3);
+            this.panel2.Controls.Add(this.outputTextBox3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 351);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(697, 156);
+            this.panel2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(295, 23);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 29);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Вывод";
+            // 
+            // inputTextBox3
+            // 
+            this.inputTextBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inputTextBox3.Location = new System.Drawing.Point(43, 57);
+            this.inputTextBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.inputTextBox3.Name = "inputTextBox3";
+            this.inputTextBox3.Size = new System.Drawing.Size(132, 34);
+            this.inputTextBox3.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(38, 23);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 29);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ввод";
+            // 
+            // startButton3
+            // 
+            this.startButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startButton3.Location = new System.Drawing.Point(43, 100);
+            this.startButton3.Margin = new System.Windows.Forms.Padding(4);
+            this.startButton3.Name = "startButton3";
+            this.startButton3.Size = new System.Drawing.Size(133, 42);
+            this.startButton3.TabIndex = 1;
+            this.startButton3.Text = "Start";
+            this.startButton3.UseVisualStyleBackColor = true;
+            // 
+            // outputTextBox3
+            // 
+            this.outputTextBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.outputTextBox3.Location = new System.Drawing.Point(301, 57);
+            this.outputTextBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.outputTextBox3.Name = "outputTextBox3";
+            this.outputTextBox3.Size = new System.Drawing.Size(184, 34);
+            this.outputTextBox3.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.inputBTextBox);
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Controls.Add(this.inputATextBox);
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Controls.Add(this.startButton2);
+            this.panel3.Controls.Add(this.outputTextBox2);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 156);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(697, 195);
+            this.panel3.TabIndex = 7;
+            // 
+            // inputATextBox
+            // 
+            this.inputATextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inputATextBox.Location = new System.Drawing.Point(43, 57);
+            this.inputATextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.inputATextBox.Name = "inputATextBox";
+            this.inputATextBox.Size = new System.Drawing.Size(132, 34);
+            this.inputATextBox.TabIndex = 0;
+            this.inputATextBox.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(38, 23);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 29);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Ввод";
+            // 
+            // startButton2
+            // 
+            this.startButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startButton2.Location = new System.Drawing.Point(44, 136);
+            this.startButton2.Margin = new System.Windows.Forms.Padding(4);
+            this.startButton2.Name = "startButton2";
+            this.startButton2.Size = new System.Drawing.Size(133, 42);
+            this.startButton2.TabIndex = 1;
+            this.startButton2.Text = "Start";
+            this.startButton2.UseVisualStyleBackColor = true;
+            this.startButton2.Click += new System.EventHandler(this.startButton2_Click);
+            // 
+            // outputTextBox2
+            // 
+            this.outputTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.outputTextBox2.Location = new System.Drawing.Point(301, 57);
+            this.outputTextBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.outputTextBox2.Multiline = true;
+            this.outputTextBox2.Name = "outputTextBox2";
+            this.outputTextBox2.Size = new System.Drawing.Size(282, 111);
+            this.outputTextBox2.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(296, 24);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 29);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Вывод";
+            // 
+            // inputBTextBox
+            // 
+            this.inputBTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inputBTextBox.Location = new System.Drawing.Point(44, 94);
+            this.inputBTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.inputBTextBox.Name = "inputBTextBox";
+            this.inputBTextBox.Size = new System.Drawing.Size(132, 34);
+            this.inputBTextBox.TabIndex = 5;
+            this.inputBTextBox.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 507);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox inputTextBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button startButton3;
+        private System.Windows.Forms.TextBox outputTextBox3;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox inputATextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button startButton2;
+        private System.Windows.Forms.TextBox outputTextBox2;
+        private System.Windows.Forms.TextBox inputBTextBox;
+    }
+}
+
---
+++ App1/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // + - сложение     S += a     S = S + a
+        // - - вычитание    S -= a     S = S - a
+        // * - умножение    S *= a     S = S * a
+        // / - деление      S /= a     S = S / a
+        // % - остаток      S %= a     S = S % a
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            string inputStr = inputTextBox.Text;
+            int inputNumber = Convert.ToInt32(inputStr);
+            int sum = 0;
+            //algorithm
+            while (inputNumber > 0)
+            {
+                if ((inputNumber % 10) % 2 == 0){
+                    sum += inputNumber % 10;
+                }
+                inputNumber = inputNumber / 10;
+            }
+            outputTextBox.Text = sum.ToString();
+        }
+
+        private void startButton2_Click(object sender, EventArgs e)
+        {
+            int A = Convert.ToInt32(inputATextBox.Text);
+            int B = Convert.ToInt32(inputBTextBox.Text);
+
+            int N = 0;
+            string output = "";
+
+            //"hello" + " " + "world" = "hello world"
+            for(int i = A; i <= B; i++){
+                output = output + i.ToString() + " ";
+                N++;
+            }
+            output = output + $" Всего: {N}";
+
+            outputTextBox2.Text = output;
+        }
+    }
+}
